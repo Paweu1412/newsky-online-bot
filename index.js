dcb904ef334a728c2baee072a21d9c4ec5c29c73
Paweł Nosalski <fordcrown2009@gmail.com>
2024-02-06 00:02:58 +0100

    Basics

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const client = new Client();
-client.on("ready", () => {
-  console.log("I am ready!");
+var BOT_TOKEN = process.env.BOT_TOKEN;
+var CHANNEL_ID = process.env.CHANNEL_ID;
+var NEWSKY_API_TOKEN = process.env.NEWSKY_API_TOKEN;
+
+const client = new Client({
+  intents: [ GatewayIntentBits.Guilds ]
 });
 
-client.login(process.env.BOT_TOKEN);
+const fetchFlights = async (callback) => {
+  const response = await fetch("https://newsky.app/api/airline-api/flights/ongoing", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${NEWSKY_API_TOKEN}`
+    }
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    callback(data.results);
+  }
+}
+
+client.on("ready", async () => {
+  const channel = client.channels.cache.get(CHANNEL_ID);
+
+  if (channel) {
+    channel.messages.fetch().then((messages) => {
+      messages.forEach((message) => {
+        message.delete();
+      });
+    });
+  }
+
+  fetchFlights((currentFlights) => {
+    console.log(currentFlights)
+
+    let embeds = [];
+
+    embeds[0] = new EmbedBuilder()
+      .setTitle("Ongoing PAFFSair flights")
+      .setColor("#00AA00")
+    
+    
+    embeds[1] = new EmbedBuilder()
+      .setColor("#FFFFFF")
+
+      
+    currentFlights.forEach((flight) => {
+      embeds[1].addFields(
+        { name: `${flight.airline.icao}${flight.flightNumber} - ${flight.pilot.fullname.split(' ')[0]}`, value: `**Dep**: ${flight.dep.icao} | **Arr**: ${flight.arr.icao} | **In flight**: ${flight.}` }
+      );
+    });
+    
+
+    channel.send({ embeds: embeds });
+  });
+});
+
+client.login(BOT_TOKEN);
